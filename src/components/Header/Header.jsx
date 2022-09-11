6a5550e16a28afcfd6b2d239d09f6e2d0a6481f3
henrymedeiros <henrymedeiros77@gmail.com>
2022-09-11 19:12:08 -0300

    feat: adding logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,8 +8,34 @@ const Header = ({ scrollDir }) => {
       }
     >
       <nav>
-        <a href="#" className={HeaderCss.logo}>
-          Henry
+        <a href="#" className={HeaderCss.logoContainer}>
+          <svg
+            width="40"
+            height="48"
+            viewBox="0 0 40 60"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0_10_89)">
+              <path
+                d="M6.24232 20.0123V5.00306L-3.05176e-05 0V20.0123L6.24232 25.0061V20.0123Z"
+                fill="#0E1212"
+              />
+              <path
+                d="M37.5 5.00306V0L31.2462 5.00306V25.0061L37.5 20.0031V5.00306Z"
+                fill="#0E1212"
+              />
+              <path
+                d="M31.2691 29.9908L18.75 40.0061L6.2538 30.0092L6.24232 30.0184V30.0092L-3.05176e-05 25.0061V55.0061L6.24232 60V37.073L14.3294 43.5426L18.75 47.0791L31.2462 37.073V59.9699L37.4781 54.9853H37.5V25.0061L31.2691 29.9908Z"
+                fill="#0E1212"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_10_89">
+                <rect width="37.5" height="60" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
         </a>
         <ul>
           <li>
