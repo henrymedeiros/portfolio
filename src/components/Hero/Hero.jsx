b516a08e319ce775788643401900f92b559ede3e
henrymedeiros <henrymedeiros77@gmail.com>
2022-09-12 01:25:27 -0300

    feat: new scroll down animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,12 +4,11 @@ import ProgrammerSVG from "./ProgrammerSVG.jsx";
 import Button from "../Button/Button.jsx";
 
 const Hero = () => {
-  
   return (
     <section id="hero" className={HeroCss.HeroSection}>
       <div className={HeroCss.HeroText}>
         <h1 className={`${HeroCss.Title} hero-title`}>Henry Medeiros</h1>
-        <h2 className={HeroCss.Subtitle}>
+        <h3 className={HeroCss.Subtitle}>
           {/*<Typewriter
             options={{
               strings: ["Desenvolvedor Front-End", "Freelancer", ],
@@ -18,7 +17,7 @@ const Hero = () => {
             }}
           />*/}
           Desenvolvedor Front-End
-        </h2>
+        </h3>
         <p className={HeroCss.Paragraph}>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla,
           beatae ea! Est, fuga quibusdam voluptate laboriosam voluptatem.
@@ -27,15 +26,8 @@ const Hero = () => {
       </div>
 
       <ProgrammerSVG color="#6E57E0" />
-
-      <svg className={HeroCss.arrows}>
-        <path className={HeroCss.a1} d="M0 0 L30 32 L60 0"></path>
-        <path className={HeroCss.a2} d="M0 20 L30 52 L60 20"></path>
-        <path className={HeroCss.a3} d="M0 40 L30 72 L60 40"></path>
-      </svg>
-      </section>
-      
-    
+      <div class={HeroCss.mouse}></div>
+    </section>
   );
 };
 
